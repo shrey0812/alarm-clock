dbe8b2fd5294ebb7bf315c04e513ba8c2b371fb2
Shrey Gupta <shreygupta0812@gmail>
2022-06-14 22:00:34 +0530

    initial commit

---
+++ app/src/main/java/com/example/alarmclock/MainActivity.java
@@ -0,0 +1,127 @@
+package com.example.alarmclock;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.alarmclock.databinding.ActivityMainBinding;
+import com.google.android.material.timepicker.MaterialTimePicker;
+import com.google.android.material.timepicker.TimeFormat;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private MaterialTimePicker picker;
+    private Calendar calendar;
+    private AlarmManager alarmManager;
+    private PendingIntent pendingIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        createNotificationChannel();
+
+        binding.selectTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                showTimePicker();
+            }
+        });
+        binding.setAnAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              setAlarm();
+
+            }
+
+
+        });
+        binding.cancelAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               cancelAlarm();
+
+            }
+        });
+    }
+    private void setAlarm() {
+        alarmManager=(AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        Intent intent= new Intent(this, AlarmReceiver.class);
+        pendingIntent=PendingIntent.getBroadcast(this,0,intent,0);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),AlarmManager.INTERVAL_DAY,pendingIntent);
+        Toast.makeText(this,"Alarm Set Successfully",Toast.LENGTH_SHORT).show();
+    }
+
+    private void cancelAlarm() {
+        Intent intent= new Intent(this, AlarmReceiver.class);
+        pendingIntent=PendingIntent.getBroadcast(this,0,intent,0);
+        if(alarmManager==null){
+            alarmManager= (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        }
+        alarmManager.cancel(pendingIntent);
+        Toast.makeText(this,"Alarm Cancelled Successfully",Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void showTimePicker(){
+
+
+        picker=new MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_12H)
+                .setHour(12)
+                .setMinute(0)
+                .setTitleText("Select Alarm time")
+                .build();
+
+        picker.show(getSupportFragmentManager(),"shreyAndroid");
+
+                picker.addOnPositiveButtonClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        if(picker.getHour()>12){
+                            binding.selectedTime.setText(String.format("%02d",(picker.getHour()-12)+" : "+String.format("%02d",picker.getMinute())+"PM"));
+                        }
+                        else{
+                            binding.selectedTime.setText((picker.getHour()+" : "+picker.getMinute()+"AM"));
+                        }
+                        calendar=Calendar.getInstance();
+                        calendar.set(Calendar.HOUR_OF_DAY,picker.getHour());
+                        calendar.set(Calendar.MINUTE,picker.getMinute());
+                        calendar.set(Calendar.SECOND,0);
+                        calendar.set(Calendar.MILLISECOND,0);
+                    }
+                });
+    }
+    public void createNotificationChannel(){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            CharSequence name="shreyAndroidReminderChannel";
+            String description="Channel for Alarm Manager";
+            int importance= NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel=new NotificationChannel("shreyAndroid",name,importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager=getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+
+}
